be2623d700e3775f833bee65794a53c384406cba
Simon Merlung <smerlung@gmail.com>
2015-05-17 01:26:38 +0200

    Initial commit

---
+++ Shade/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Shade.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shade
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string shadetext = ShadeStream.GetShadeUnicodeText(@"C:\Development\CSharp\Shade\shadeimage.png");
+
+            ShadeStream.SetShadeUnicodeText(@"C:\Development\CSharp\Shade\shadeimage.png", "Simon Merlung");
+        }
+    }
+}
---
+++ Shade/Objects/ShadeStream.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Shade.Objects
+{
+    public class ShadeStream
+    {
+        public static string GetShadeUnicodeText(string imagepath)
+        {
+            byte[] bytes = GetImageBytes(imagepath);
+            byte[] shade = GetShadeBytes(bytes);
+            return UnicodeEncoding.UTF8.GetString(shade);
+        }
+
+        public static void SetShadeUnicodeText(string imagepath, string unicode)
+        {
+            byte[] data = UnicodeEncoding.UTF8.GetBytes(unicode);
+            byte[] bytes = GetImageBytes(imagepath);
+            SetShadeBytes(bytes, data);
+            SetImageBytes(imagepath, bytes);
+        }
+
+        public static byte[] GetShadeBytes(IList<byte> data)
+        {
+            List<byte> result = new List<byte>();
+            for (int i = 0; i < data.Count - 8; i+=8)
+            {
+                byte value = GetShadeByte(data, i);
+                result.Add(value);
+            }
+
+            return result.ToArray();
+        }
+
+        public static void SetShadeBytes(IList<byte> data, IList<byte> shadebytes)
+        {
+            for (int i = 0; i < shadebytes.Count; i += 8)
+            {
+                SetShadeByte(data, i, shadebytes[i]);
+            }
+        }
+
+        public static byte GetShadeByte(IList<byte> data, int index)
+        {
+            byte result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                byte bit = (byte)(data[index + i] & 0x1);
+                result <<= 1;
+                result |= bit;
+            }
+
+            return result;
+        }
+
+        public static void SetShadeByte(IList<byte> data, int index, byte value)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                byte clearedbyte = (byte)(data[index + i] & 0xfe);
+                byte bit = (byte)((value >> 7 - i) & 0x1);
+                clearedbyte |= bit;
+                data[index + i] = clearedbyte;
+            }
+        }
+
+        public static byte[] GetImageBytes(string imagepath)
+        {
+            using (Bitmap bitmap = new Bitmap(imagepath))
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, ImageFormat.Png);
+                    return stream.GetBuffer();
+                }
+            }
+        }
+
+        public static void SetImageBytes(string imagepath, IList<byte> bytes)
+        {
+            using (MemoryStream stream = new MemoryStream(bytes.ToArray()))
+            {
+                using (Bitmap bitmap = new Bitmap(stream))
+                {
+                    bitmap.Save(imagepath, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
