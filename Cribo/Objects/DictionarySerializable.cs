43ee9847806a7bf1d6823b815367fa4a1ad3b496
Simon Merlung <smerlung@gmail.com>
2017-11-28 11:27:12 +0100

    Adding DictionarySerializable and tests

---
+++ Cribo/Objects/DictionarySerializable.cs
@@ -0,0 +1,63 @@
+﻿namespace Cribo.Objects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DictionarySerializable<TKey, TValue>
+    {
+        private bool isdictionaryupdated = true;
+
+        private Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+        private List<TKey> keys = new List<TKey>();
+        private List<TValue> values = new List<TValue>();
+
+        public List<TKey> Keys { get { return this.keys; } set { this.keys = value; this.UpdateDictionary(); } }
+
+        public List<TValue> Values { get { return this.values; } set { this.values = value; this.UpdateDictionary(); } }
+
+        public void Add(TKey key, TValue value)
+        {
+            this.dictionary.Add(key, value);
+            this.UpdateKeysValues();
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (this.isdictionaryupdated)
+                {
+                    return this.dictionary[key];
+                }
+                else
+                {
+                    throw new Exception("Dictionary is not updated");
+                }
+            }
+            set { this.dictionary[key] = value; this.UpdateKeysValues(); }
+        }
+
+        private void UpdateKeysValues()
+        {
+            this.keys = this.dictionary.Keys.ToList();
+            this.values = this.dictionary.Values.ToList();
+            this.isdictionaryupdated = true;
+        }
+
+        private void UpdateDictionary()
+        {
+            if (this.keys.Count != this.values.Count)
+            {
+                this.isdictionaryupdated = false;
+            }
+            else
+            {
+                this.dictionary = Enumerable.Range(0, this.keys.Count).ToDictionary(i => this.keys[i], i => this.values[i]);
+                this.isdictionaryupdated = true;
+            }
+        }
+    }
+}
